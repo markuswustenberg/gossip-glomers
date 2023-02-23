578c7160897ff9230d4c8cfd29eab22d1efb0fe6
Markus Wüstenberg <markus@maragu.dk>
2023-02-23 14:16:46 +0100

    Add solution for Single-Node Broadcast challenge

--- Makefile
+++ Makefile
@@ -7,3 +7,8 @@ echo:
 unique-ids:
 	go build ./cmd/maelstrom-unique-ids
 	cd maelstrom && ./maelstrom test -w unique-ids --bin ${PWD}/maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+
+.PHONY: broadcast-single
+broadcast-single:
+	go build ./cmd/maelstrom-broadcast
+	cd maelstrom && ./maelstrom test -w broadcast --bin ${PWD}/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10
---
+++ cmd/maelstrom-broadcast/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	var messages []any
+	mutex := sync.RWMutex{}
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		mutex.Lock()
+		defer mutex.Unlock()
+		messages = append(messages, body["message"])
+
+		body["type"] = "broadcast_ok"
+		delete(body, "message")
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		mutex.RLock()
+		defer mutex.RUnlock()
+
+		body["type"] = "read_ok"
+		body["messages"] = messages
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		delete(body, "topology")
+		body["type"] = "topology_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
